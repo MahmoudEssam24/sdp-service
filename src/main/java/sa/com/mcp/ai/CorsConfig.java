fb94f83733a38dfa316b79587e466b575a2c21e9
Mahmoud Essam <mahmoudessam24@gmail.com>
2025-11-26 22:32:58 +0300

    updated cors

--- src/main/java/sa/com/mcp/ai/CorsConfig.java
+++ src/main/java/sa/com/mcp/ai/CorsConfig.java
@@ -8,19 +8,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-          .allowedOrigins(
-            "https://19e28ad4-59b8-4219-a686-3b065d303788.lovableproject.com"
-          )
-          .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-          .allowedHeaders("*")
-          .allowCredentials(true);
-      }
-    };
-  }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://19e28ad4-59b8-4219-a686-3b065d303788.lovableproject.com",
+                                "https://id-preview--19e28ad4-59b8-4219-a686-3b065d303788.lovable.app"
+
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
