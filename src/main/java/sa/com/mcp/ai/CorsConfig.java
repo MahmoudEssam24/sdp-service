8e11ac8acfa6b6ee3623601dff74fdf74a38171f
Mahmoud Essam <mahmoudessam24@gmail.com>
2025-11-26 22:21:49 +0300

    adding cors config class

---
+++ src/main/java/sa/com/mcp/ai/CorsConfig.java
@@ -0,0 +1,26 @@
+package sa.com.mcp.ai;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+          .allowedOrigins(
+            "https://19e28ad4-59b8-4219-a686-3b065d303788.lovableproject.com"
+          )
+          .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+          .allowedHeaders("*")
+          .allowCredentials(true);
+      }
+    };
+  }
+}
